fd223f94c941e203e96fd949d1297f51a0367384
JessCucumber <104041115+JessCucumber@users.noreply.github.com>
2024-04-08 00:25:57 -0400

    Add files via upload

--- video.js
+++ video.js
@@ -1,5 +1,4 @@
 var video;
-var playbackRate = 1.0; 
 video=document.querySelector("#player1")
 
 window.addEventListener("load", function() {
@@ -7,22 +6,21 @@ window.addEventListener("load", function() {
 	video.autoplay = false;
 	video.loop = false;
 	video.load();
-	console.log("Auto play is set to" + video.autoplay)
-	console.log ("loop is set to " + video.loop)
 });
 
 
 //play
 document.querySelector("#play").addEventListener("click", function() {
-	video.play();
 	console.log("Play Video");
+	video.play();
+
 });
 
 
 //pause
 document.querySelector("#pause").addEventListener("click", function() {
-	video.pause();
 	console.log("Pause Video");
+	video.pause();
 });
 
 
@@ -31,47 +29,43 @@ document.querySelector("#slower").addEventListener("click", function() {
 	console.log("Slow Down");
 
 	//function to slow it down
-	playbackRate -= 0.1;
-	video.playbackRate = playbackRate;
-
-	console.log(playbackRate);
+	video.playbackRate = video.playbackRate-.10;
+	console.log(video.playbackRate);
 });
 
 //I am SPEED
 //hints from Jackie during class :look at the video for playback speed - 10%
 document.querySelector("#faster").addEventListener("click", function() {
 	console.log("Speed Up");
-	playbackRate += 0.1;
-	video.playbackRate = playbackRate;
-	console.log(playbackRate)
+	video.playbackRate = video.playbackRate+.10;
+	console.log(video.playbackRate);
 });
 
 
 //skippidy-bop-bop (10 seconds)
 document.querySelector("#skip").addEventListener("click", function() {
 	console.log("Skip Ahead");
-	video.currentTime += 10;
+	video.currentTime = video.currentTime + 10;
 	//this function above =  10 seconds//
 });
 
 // mute
 document.querySelector("#mute").addEventListener("click", function() {
     console.log("Mute");
-    video.muted = !video.muted; // Toggle mute state
+    video.muted = !video.muted; 
 });
 
 //change volume (slider thingy)
-document.querySelector("#slider").addEventListener("input", function() {
-    console.log("Volume Change");
-    var volume = parseFloat(this.value) / 100; // Convert slider value to volume value
-    video.volume = volume; // Set video volume
-	updateVolumeDisplay();
+document.querySelector("#slider").addEventListener("change", function() {
+	video.volume = this.value / 100;
+		document.querySelector("#volume").innerHTML = video.volume * 100 + "%";
+		console.log("Vintage style")
 
 });
 
 
 // old school vibes
-document.querySelector("vintage").addEventListener("click", function() {
+document.querySelector("#vintage").addEventListener("click", function() {
 	video.classList.add("oldSchool");
 	console.log("Vintage Style")
 
